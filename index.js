a43f3c119a4a2e3f4b59718ba89cd5e90b6a8759
Samira <58561180+Samira-hm-a@users.noreply.github.com>
2020-11-04 11:30:54 +0100

    sec commit

--- index.js
+++ index.js
@@ -5,33 +5,27 @@ app.set('views', './views');
 app.set('view engine', 'pug')
 
 app.use(express.static('public'))//Where to serve static content
-
 //verify Date
+
 app.use(verifDate = (req, res, next) => {
     const date = new Date();
-    const day = date.toDateString().substring(0,9);
+    const day = date.toDateString().substring(0,3);
     const hour = date.toTimeString().substring(0,2);
     // console.log(hour)
-    let temp = false;
+    let t = false;
     switch (day) {
-        case 'Monday':
-        case 'Tuesday':
-        case 'Wednesday':
-        case 'Thursday':
-        case 'Friday': temp = true
+        case 'Mon':
+        case 'Tue':
+        case 'Wed':
+        case 'Thu':
+        case 'Fri': t = true
     }
-    if (temp === true && hour <= 17 && hour >= 9){
+    if (t === true && hour <= 17 && hour >= 9){
         console.log('we are on working time welcome!')
         next();
     }else res.send("we are off working time, available from monday to friday from 9 a.m to 5 p.m.")
     });                         
-    // if (date.getDay()==7 || date.getHours()<9 || date.getHours()>17)
-    //   {
-    //       return next (new Error)
-    //   } else {
-    //       console.log('we are on working time welcome!');
-    //       return next();
-    
+
 app.get('/', (req,res) => {
     res.render('home')
 });
